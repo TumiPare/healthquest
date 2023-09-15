1a9a7f695f299368865f934e1672374e12895f7c
Amber-Leigh Lezar <u18004874@tuks.co.za>
2023-09-15 20:03:05 +0200

    Add water intake page

--- app/HealthQuest/src/app/waterIntake/water-intake/water-intake.page.ts
+++ app/HealthQuest/src/app/waterIntake/water-intake/water-intake.page.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./water-intake.page.scss'],
 })
 export class WaterIntakePage implements OnInit {
+  water!: number;
+  waterError = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
 
+  submitWater() {
+    if (!isNaN(this.water) && this.water >= 1 && this.water <= 50) {
+      console.log('Valid weight:', this.water);
+      this.waterError = false;
+    } else {
+      console.log('Invalid amount. Please enter a number between 1 and 50.');
+      this.waterError = true;
+    }
+  }
+
+  onWaterChange() {
+    this.waterError = false;
+  }
 }
