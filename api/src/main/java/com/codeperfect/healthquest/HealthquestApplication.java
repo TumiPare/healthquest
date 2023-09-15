616c8d7183ec1c51e79159f45c51fd2873a6cc8e
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-09-15 12:11:44 +0200

    🎉 (api) Setup MongoDB Connection

--- api/src/main/java/com/codeperfect/healthquest/HealthquestApplication.java
+++ api/src/main/java/com/codeperfect/healthquest/HealthquestApplication.java
@@ -2,8 +2,10 @@ package com.codeperfect.healthquest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class HealthquestApplication {
 
 	public static void main(String[] args) {
