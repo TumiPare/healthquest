8744d620ae3d482b523b13d1c39462e3c40e6c64
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-09-15 10:58:53 +0200

    🎉 (app) API Structure Setup

---
+++ api/src/main/java/com/codeperfect/healthquest/HealthquestApplication.java
@@ -0,0 +1,13 @@
+package com.codeperfect.healthquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthquestApplication.class, args);
+	}
+
+}
