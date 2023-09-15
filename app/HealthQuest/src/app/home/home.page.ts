7289ecfe3e2bfd119cba6a17decefb300af82aed
AmberLz <u18004874@tuks.co.za>
2023-09-15 15:46:40 +0200

    Added carousel

--- app/HealthQuest/src/app/home/home.page.ts
+++ app/HealthQuest/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,4 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() { }
-
 }
