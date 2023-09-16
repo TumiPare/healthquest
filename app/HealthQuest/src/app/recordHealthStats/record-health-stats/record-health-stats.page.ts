1a03cb9f678dccabcba6c511b7baf21533161222
Amber-Leigh Lezar <u18004874@tuks.co.za>
2023-09-16 15:28:44 +0200

    Create form

--- app/HealthQuest/src/app/recordHealthStats/record-health-stats/record-health-stats.page.ts
+++ app/HealthQuest/src/app/recordHealthStats/record-health-stats/record-health-stats.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-record-health-stats',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record-health-stats.page.scss'],
 })
 export class RecordHealthStatsPage implements OnInit {
+  healthForm: FormGroup = this.formBuilder.group({
+    steps: ['', [Validators.required, Validators.min(0)]],
+    weight: ['', [Validators.required, Validators.min(2), Validators.max(500)]],
+    water: ['', [Validators.required, Validators.email]],
+    food: ['', [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
   }
 
+  submitHealthStats() {
+    if (this.healthForm.valid) {
+      console.log("valid form");
+    }
+    else {
+    this.failToast('Please ensure all details are correct');
+    }
+  }
+
+  async failToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      color: 'danger',
+      duration: 3000,
+      position: 'bottom',
+    });
+    toast.present();
+  }
 }
