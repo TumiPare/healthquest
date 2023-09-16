1ae4ce72f6c480c4f4926ea2a6ef43422ad037c1
Amber-Leigh Lezar <u18004874@tuks.co.za>
2023-09-16 14:23:09 +0200

    Update peer compare

--- app/HealthQuest/src/app/peerCompare/peer-compare/peer-compare.page.ts
+++ app/HealthQuest/src/app/peerCompare/peer-compare/peer-compare.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js/auto'
+
+Chart.register(...registerables)
 
 @Component({
   selector: 'app-peer-compare',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./peer-compare.page.scss'],
 })
 export class PeerComparePage implements OnInit {
+  @ViewChild('doughnutChart') doughnutChartRef!: ElementRef;
 
+  doughnutChart: any = null;
+  mySteps: number = 2000;
+  peerSteps: number = 1000;
   constructor() { }
 
   ngOnInit() {
+    this.processDoughnutChart();
   }
 
+  processDoughnutChart() {
+    // Data for Doughnut Chart (Uptime/Downtime for Today)
+    const doughnutData = {
+      labels: ["My Steps", "John's Steps"],
+      datasets: [{
+        label: 'Loadshedding',
+        data: [this.mySteps, this.peerSteps], // Uptime vs Downtime
+        borderWidth: 0,
+      }]
+    };
+    this.populateDoughnutChart(doughnutData);
+  }
+
+  populateDoughnutChart(doughnutData: any) {
+    if (this.doughnutChart) {
+      this.doughnutChart.clear();
+      this.doughnutChart.destroy();
+    }
+
+    this.doughnutChart = new Chart("doughnutChart", {
+      type: 'doughnut',
+      data: doughnutData,
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        }
+      }
+    });
+  }
 }
