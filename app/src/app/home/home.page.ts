b56fa5df6c41ec99dd2b23bbe8c8073513532fd0
Ryan Trickett <ryanbasiltrickett@gmail.com>
2023-09-17 08:15:53 +0200

    🐛 (app) Small Bug Fixes

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,41 +1,5 @@
 import { Component } from '@angular/core';
 import { register } from 'swiper/element/bundle';
-import { NavController } from '@ionic/angular';
-
-register();
-
-@Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
-})
-export class HomePage {
-
-  constructor(private navCtrl: NavController) {}
-
-  redirectToStepsTracking()
-  {
-    this.navCtrl.navigateForward('/steps-tracking'); 
-  }
-
-  redirectToWeightEntry()
-  {
-    this.navCtrl.navigateForward('/weight-entry'); 
-  }
-
-  redirectToFoodIntake()
-  {
-    this.navCtrl.navigateForward('/food-intake'); 
-  }
-
-  redirectToWaterIntake()
-  {
-    this.navCtrl.navigateForward('/water-intake'); 
-  }
-}
-
-import { Component } from '@angular/core';
-import { register } from 'swiper/element/bundle';
 import { ModalController, NavController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { IChallenge } from '../user/challenge.interface';
