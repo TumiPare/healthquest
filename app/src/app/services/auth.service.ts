b78d356f5ce82c4cc510bf6035d5f9d16b279f70
Andreas Visagie u21430901 <angryaxe.visagie@gmail.com>
2023-09-17 08:42:33 +0200

    Logout

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserStorage } from '../user/user.storage';
 import { IUser } from '../profile/user.interface';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class AuthService {
   apiUrl = 'http://localhost:5000/auth/'
   user: IUser | null = null;
 
-  constructor(private http: HttpClient, private userStorage: UserStorage) { }
+  constructor(private http: HttpClient, private userStorage: UserStorage, private router: Router) { }
 
   signup(username: string, password: string, weight: number, height: number, dob: string) {
     
@@ -37,6 +38,11 @@ export class AuthService {
     }
 
     return this.http.post(this.apiUrl+'login', loginUser)
+  }
+
+  logout() {
+    this.userStorage.user = null;
+    this.router.navigate(['']);
   }
 
 }
