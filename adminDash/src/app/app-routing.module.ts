67551af10628429cd566cc5b6fc47dbbb21e8a5a
Amber-Leigh Lezar <u18004874@tuks.co.za>
2023-09-23 09:15:39 +0200

    Create dashboard page

--- adminDash/src/app/app-routing.module.ts
+++ adminDash/src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
