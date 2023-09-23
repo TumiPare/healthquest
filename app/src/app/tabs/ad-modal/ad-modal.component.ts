8c008b835ea07852fc2803429c0d2c72b8941715
Andreas Visagie u21430901 <angryaxe.visagie@gmail.com>
2023-09-23 14:14:14 +0200

    daddy longdi

---
+++ app/src/app/tabs/ad-modal/ad-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ad-modal',
+  templateUrl: './ad-modal.component.html',
+  styleUrls: ['./ad-modal.component.scss'],
+})
+export class AdModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- app/src/app/tabs/tabs.page.ts
+++ app/src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tabs',
@@ -8,9 +9,23 @@ import { Router } from '@angular/router';
 })
 export class TabsPage {
 
-  constructor(private router: Router) {}
+  userType: string = "";
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+   this.authService.typeUser.subscribe((typeUser) => {
+     this.userType = typeUser;
+   });
+  }
 
   navigateToRecordHealthStats() {
     this.router.navigate(['/record-health-stats']);
+  }
+
+  showAd() {
+    if(this.userType != 'premium') {
+      console.log('show ad');
+    }
   }
 }
